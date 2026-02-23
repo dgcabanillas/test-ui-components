5a519a35a1ae861dfd552a28a620da3ddfbb254d
Diego Cabanillas <diego.cabanillas@spsa.pe>
2026-02-23 18:10:34 -0500

    setting up button onclick

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 type ButtonProps = {
 	children: React.ReactNode;
+	onClick?: () => void;
 };
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({ children, onClick = () => {} }: ButtonProps) => {
+	const handleClick = () => {
+		console.log('@indigitalxp/Button clicked!');
+		onClick();
+	}
+
 	return (
-		<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+		<button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 			Custom Button { children }
 		</button>
 	);
